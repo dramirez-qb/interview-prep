62d4c9e476722a32eb3ae68dc05913090effc32b
guajardo <a282057@uach.mx>
2018-02-26 10:43:22 -0700

    Add observer pattern

---
+++ design_patterns/observer/employee_v4.rb
@@ -0,0 +1,20 @@
+require_relative 'subject'
+
+class Employee
+  include Subject
+   
+  attr_reader :name, :title
+  attr_reader :salary
+
+  def initialize(name, title, salary)
+    super()
+    @name = name
+    @title = title
+    @salary = salary
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    notify_observers
+  end
+end
---
+++ design_patterns/observer/employee_v5.rb
@@ -0,0 +1,23 @@
+require 'observer'
+
+class Employee
+  include Observable
+
+  attr_reader :name, :title
+  attr_reader :salary
+
+  def initialize(name, title, salary)
+    @name = name
+    @title = title
+    @salary = salary
+  end
+
+  def salary=(new_salary)
+    old_salary = @salary
+    @salary = new_salary
+    if old_salary != new_salary
+      changed
+      notify_observers(self)
+    end
+  end
+end
